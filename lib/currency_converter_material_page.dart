668cbe8913862130e8e0953f1d63bc3828f5cc1c
Seonyoung Pang <seonyoungpang@Seonyoungs-Laptop.local>
2024-11-05 20:33:18 +0900

    update UI with textbutton widget

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -6,43 +6,62 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    const border = OutlineInputBorder(
+                      //Color(0xAARRGGBB) alpha , red , green, black
+                      //0xFF000000
+                      borderSide: BorderSide(
+                        width: 2.0,
+                      )
+                    );
+
+    return Scaffold(
         backgroundColor: Colors.green,
         body : Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-               Text('O' , 
+               const Text('O' , 
                style: TextStyle(
                 fontSize: 85,
                 fontWeight: FontWeight.bold,
                 color : Colors.white
                )
                ),
-               TextField(
-                style: TextStyle(
-                  color: Colors.white
-                  ),
-                  decoration: InputDecoration(
-                    hintText: 'Please enter amount in KRW',
-                    hintStyle: TextStyle(
-                      color : Colors.black
-                    ),
-                    prefixIcon: Icon(
-                      Icons.monetization_on , 
-                      color: Colors.black
+               //padding and container
+               //contrl + shift + R : refactoring widget
+               const Padding(
+                 padding: EdgeInsets.all(8.0),
+                 child: TextField(
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(20.0),
+                      hintText: 'Please enter amount in KRW',
+                      hintStyle: TextStyle(
+                        color : Colors.black
                       ),
-                    filled : true,
-                    fillColor: Colors.white38,
-                    focusedBorder: OutlineInputBorder(
-                      //Color(0xAARRGGBB) alpha , red , green, black
-                      //0xFF000000
-                      borderSide: BorderSide(
-                        width: 2.0,
-                      )
-                    )
-                  ),
+                      prefixIcon: Icon(
+                        Icons.monetization_on, 
+                        color: Colors.black
+                        ),
+                      filled : true,
+                      fillColor: Colors.white38,
+                      focusedBorder: border,
+                      enabledBorder: border
+                    ),
+                    keyboardType: TextInputType.numberWithOptions(
+                      decimal: true,
+                      signed: true
+                    ),
+                 ),
                ),
+               //Button
+
+               //raised
+               //appears like a text
+               TextButton(
+                onPressed: () {
+                  print('button clicked');
+                }, 
+                child: const Text('Click Here!'))
           ],
           ),
         ),
