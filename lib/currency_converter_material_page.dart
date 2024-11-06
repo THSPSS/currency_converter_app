b45e5a1dea0f6a2d9ade21b55c6061757cf084bd
Seonyoung Pang <seonyoungpang@Seonyoungs-Laptop.local>
2024-11-06 11:59:40 +0900

    update convert logic on textButton widget

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-class CurrencyConverterMaterialPage extends StatelessWidget {
+class CurrencyConverterMaterialPage extends StatefulWidget {
   const CurrencyConverterMaterialPage({super.key});
+
+  @override
+  State<CurrencyConverterMaterialPage> createState() => _CurrencyConverterMaterialPageState();
+}
+
+class _CurrencyConverterMaterialPageState extends State<CurrencyConverterMaterialPage> {
+
+  // Create a text controller and use it to retrieve the current value
+  // of the TextField.
+  final inputAmountController = TextEditingController();
+  String convertedToAmount = '0';
+
+  @override
+  void dispose() {
+    inputAmountController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,14 +30,16 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                       )
                     );
 
+
     return Scaffold(
         backgroundColor: Colors.green,
         body : Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-               const Text('O' , 
-               style: TextStyle(
+               Text(
+                convertedToAmount, 
+                style: const TextStyle(
                 fontSize: 85,
                 fontWeight: FontWeight.bold,
                 color : Colors.white
@@ -29,10 +47,11 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                ),
                //padding and container
                //contrl + shift + R : refactoring widget
-               const Padding(
-                 padding: EdgeInsets.all(8.0),
+               Padding(
+                 padding: const EdgeInsets.all(8.0),
                  child: TextField(
-                    decoration: InputDecoration(
+                    controller: inputAmountController,
+                    decoration: const InputDecoration(
                       contentPadding: EdgeInsets.all(20.0),
                       hintText: 'Please enter amount in KRW',
                       hintStyle: TextStyle(
@@ -47,7 +66,7 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                       focusedBorder: border,
                       enabledBorder: border
                     ),
-                    keyboardType: TextInputType.numberWithOptions(
+                    keyboardType: const TextInputType.numberWithOptions(
                       decimal: true,
                       signed: true
                     ),
@@ -59,7 +78,13 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                //appears like a text
                TextButton(
                 onPressed: () {
-                  print('button clicked');
+                  // get the value of inputAmontController.text and multiply it with usd 0.00072 
+                  var resultOfConv = (int.parse(inputAmountController.text)*0.00072).toStringAsFixed(2);
+                  // update text variable
+                  setState(() {
+                    convertedToAmount = resultOfConv;
+                  });
+
                 }, 
                 child: const Text('Click Here!'))
           ],
